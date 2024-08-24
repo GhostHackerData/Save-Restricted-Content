eb80da14fa916548960b3c5357025c467cc06a02
G̷h̷o̷s̷t̷ ̷o̷f̷ ̷D̷e̷a̷t̷h̷ <88895560+GhostHackerData@users.noreply.github.com>
2024-08-24 14:47:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "26303261"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "e924187728893dd49765079342ea3d21")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Cicada:<db_password>@cicada.nho1f.mongodb.net/?retryWrites=true&w=majority&appName=Cicada")
